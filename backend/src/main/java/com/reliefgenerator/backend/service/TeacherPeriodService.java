a5f5b2f6360dd0303fc183e62371f4df64f8bdce
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-22 12:46:54 +0530

    Update TeacherPeriodService.java

--- backend/src/main/java/com/reliefgenerator/backend/service/TeacherPeriodService.java
+++ backend/src/main/java/com/reliefgenerator/backend/service/TeacherPeriodService.java
@@ -1,12 +1,13 @@
 package com.reliefgenerator.backend.service;
 
-import com.reliefgenerator.backend.entity.TeacherPeriod;
-import com.reliefgenerator.backend.entity.Weekday;
+import com.reliefgenerator.backend.entity.*;
 import com.reliefgenerator.backend.repository.TeacherPeriodRepository;
 import com.reliefgenerator.backend.repository.WeekdayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,11 +15,43 @@ public class TeacherPeriodService {
     @Autowired
     private TeacherPeriodRepository teacherPeriodRepository;
 
+    @Lazy
+    @Autowired
+    private TeacherService teacherService;
+    @Autowired
+    private PeriodService periodService;
+    @Autowired
+    private WeekdayService weekdayService;
+
     public List<TeacherPeriod> getAllTeacherPeriods() {
         return teacherPeriodRepository.findAll();
     }
 
     public TeacherPeriod saveTeacherPeriod(TeacherPeriod teacherPeriod) {
         return teacherPeriodRepository.save(teacherPeriod);
+    }
+
+    public List<TeacherPeriod> generateAllTeacherPeriods(){
+        List<Teacher> teachers = teacherService.getAllTeachers();
+        List<Weekday> weekdays = weekdayService.getAllWeekdays();
+        List<Period> periods = periodService.getAllPeriods();
+
+        List<TeacherPeriod> teacherPeriods = new ArrayList<>();
+
+        for (Teacher teacher : teachers) {
+            for (Weekday weekday : weekdays) {
+                for (Period period : periods) {
+                    TeacherPeriod teacherPeriod = new TeacherPeriod();
+                    teacherPeriod.setTeacher(teacher);
+                    teacherPeriod.setWeekday(weekday);
+                    teacherPeriod.setPeriod(period);
+                    teacherPeriod.setIs_free(true);
+
+                    teacherPeriods.add(teacherPeriod);
+                }
+            }
+        }
+        teacherPeriodRepository.saveAll(teacherPeriods);
+        return teacherPeriods;
     }
 }
